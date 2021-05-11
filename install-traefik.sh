bb2f8179dad291c4ffb9f79a87368d20a8465d09
Nifemi Alpine Durin <k47sec@gmail.com>
2021-05-11 19:02:17 +0100

    fix

--- install-traefik.sh
+++ install-traefik.sh
@@ -1,4 +1,4 @@
 helm repo add stable/traefik https://helm.traefik.io/traefik
 helm repo update
 
-helm upgrade --install traefik traefik/traefik --set dashboard.enabled=false,dashboard.domain=dashboard.traefik,rbac.enabled=true --set kubernetes.ingressClass=traefik --namespace kube-system
+helm upgrade --install traefik traefik/traefik --set dashboard.enabled=false,dashboard.domain=dashboard.traefik,rbac.enabled=true --set kubernetes.ingressClass=traefik #--namespace kube-system
